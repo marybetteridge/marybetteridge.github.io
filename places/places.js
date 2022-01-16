cc918853fcd90dd811c2dbc71d40b1ecb60f983e
David Betteridge <davidabetteridge@gmail.com>
2022-01-16 11:32:32 +0000

    Some places

--- places/places.js
+++ places/places.js
@@ -1,6 +1,15 @@
 const data = [
-    { "lat": "53.9600", "long": "-1.0873", "colour": "#ff0000", "name": "Place 1" },
-    { "lat": "51.5072", "long": "-0.1276", "colour": "#00ff00", "name": "Place 2" },
+    { "lat": "53.9600", "long": "-1.0873", "colour": "#00ff00", "name": "York" },
+    { "lat": "53.8007", "long": "-1.5497", "colour": "#00ff00", "name": "Leeds" },
+    { "lat": "53.7945", "long": "-1.7494", "colour": "#00ff00", "name": "Bradford" },
+    { "lat": "53.7676", "long": "-0.3274", "colour": "#00ff00", "name": "Hull" },
+    { "lat": "54.2831", "long": "-0.3997", "colour": "#00ff00", "name": "Scarbrough" },
+    { "lat": "54.4886", "long": "-0.6133", "colour": "#00ff00", "name": "Whitby" },
+    { "lat": "54.5208", "long": "-1.2197", "colour": "#00ff00", "name": "Middlesbrough" },
+    { "lat": "54.9782", "long": "-1.6177", "colour": "#00ff00", "name": "Newcastle" },
+    { "lat": "55.9532", "long": "-3.1882", "colour": "#0000ff", "name": "Edinburgh" },
+    { "lat": "55.8446", "long": "-4.2403", "colour": "#0000ff", "name": "Glasgow" },
+    { "lat": "51.5072", "long": "-0.1276", "colour": "#00ff00", "name": "London" },
 ]
 
 function loadMapScenario() {
@@ -17,7 +26,7 @@ function loadMapScenario() {
             place.long)
 
         var pin = new Microsoft.Maps.Pushpin(loc, {
-            title: '',
+            title: place.name,
             subTitle: '',
             text: '',
             color: place.colour
