62578e9711a54e03e91635122f45143535a6ddbe
marybetteridge <96747584+marybetteridge@users.noreply.github.com>
2022-07-26 22:17:42 +0100

    ice ice

--- walks/walks.js
+++ walks/walks.js
@@ -7,7 +7,7 @@ function loadMapScenario() {
         /* No need to set credentials if already passed in URL */
         center: new Microsoft.Maps.Location(53.4808, -1.2426),
         mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-        zoom: 5
+        zoom: 10
     });
 
     layer = new Microsoft.Maps.Layer();
